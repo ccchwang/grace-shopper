6ccc7c0af99740c026d38b1b8e33ac2e11c7a752
Chloe Hwang <c.chloeh@gmail.com>
2017-02-28 15:44:06 -0500

    finished app container w/ navbar and footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Col } from 'react-bootstrap'
+
+
+export default function(props) {
+    return (
+      <div className="footer">
+        <Col sm={12} >
+          <Col md={4} >
+          Shop our cuties
+          </Col>
+          <Col md={4} >
+          Get to know us
+          </Col>
+          <Col md={4} >
+          Contact us
+          </Col>
+        </Col>
+      </div>
+    )
+}
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, FormControl, Button } from 'react-bootstrap'
+
+
+export default function(props) {
+    return (
+      <Navbar inverse collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="#">Cuties</a>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+        <Navbar.Collapse>
+          <Nav>
+            <NavItem eventKey={1} href="#">Link</NavItem>
+            <NavDropdown eventKey={2} title="Shop" id="basic-nav-dropdown">
+              <MenuItem eventKey={2.1}>Kittens</MenuItem>
+              <MenuItem eventKey={2.2}>Puppies</MenuItem>
+              <MenuItem eventKey={2.3}>Piglets</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={2.3}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+
+          <Nav pullRight>
+            <NavItem eventKey={1} href="#">Login</NavItem>
+            <NavItem eventKey={2} href="#">Sign Up</NavItem>
+          </Nav>
+
+          <Nav pullRight>
+            <Navbar.Form>
+              <FormGroup>
+                <FormControl type="text" placeholder="Search" />
+              </FormGroup>
+              {' '}
+              <Button type="submit">Submit</Button>
+            </Navbar.Form>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+}
