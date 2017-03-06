87eb69b67312132e3d942ee4e08c48addc7c5b0b
Chloe Hwang <c.chloeh@gmail.com>
2017-03-06 18:30:24 -0500

    created admin component

---
+++ app/components/AdminEditForm.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Form, FieldGroup, FormGroup, ControlLabel, FormControl, Button, HelpBlock } from 'react-bootstrap'
+
+export default function({ handleInputCheck, invalidName, invalidDescript, invalidCategory, invalidPrice }) {
+
+  return (
+    <div>
+      <h2>Add a New Baby</h2>
+      <Form onSubmit={handleInputCheck}>
+
+      <FormGroup validationState={invalidName}>
+        <ControlLabel>Name</ControlLabel>
+        {' '}
+        <FormControl name="name" type="text" placeholder="Fuzzy" />
+        { invalidName && <HelpBlock>Name cannot be blank.</HelpBlock> }
+        <FormControl.Feedback />
+      </FormGroup>
+      {' '}
+      <FormGroup validationState={invalidPrice}>
+        <ControlLabel>Price</ControlLabel>
+        {' '}
+        <FormControl name="price" type="text" placeholder="$100.00" />
+        { invalidPrice && <HelpBlock>Price cannot be blank.</HelpBlock> }<FormControl.Feedback />
+      </FormGroup>
+      {' '}
+      <FormGroup validationState={invalidCategory}>
+        <ControlLabel>Category</ControlLabel>
+        {' '}
+        <FormControl name="category" type="text" placeholder="piglet" />
+        <HelpBlock>Category must be lowercase.</HelpBlock>
+        { invalidCategory && <HelpBlock>Category cannot be blank.</HelpBlock> }
+      </FormGroup>
+      {' '}
+      <FormGroup validationState={invalidDescript}>
+        <ControlLabel>Description</ControlLabel>
+        <FormControl name="description" componentClass="textarea" placeholder="Best baby ever!" />
+        { invalidDescript && <HelpBlock>Description cannot be blank.</HelpBlock> }
+      </FormGroup>
+
+      <FormGroup>
+        <ControlLabel>Image URL</ControlLabel>
+        {' '}
+        <FormControl name="imgUrl" type="text" placeholder="wwww.cute.com" />
+      </FormGroup>
+      {' '}
+
+      <Button type="submit" bsStyle="success">
+        Add Product
+      </Button>
+    </Form>
+  </div>
+  )
+}
+
+
+/**
+
+ photos
+ categories
+ */
--- app/main.jsx
+++ app/main.jsx
@@ -14,6 +14,7 @@ import AppContainer from './containers/AppContainer'
 import SingleProductContainer from './containers/SingleProductContainer'
 import CartContainer from './containers/CartContainer'
 import OrderContainer from './containers/OrderContainer'
+import AdminContainer from './containers/AdminContainer'
 
 //redux things
 import { receiveProducts, receiveProduct } from './reducers/products'
@@ -66,6 +67,7 @@ render (
         <Route path="/category/:categoryName" component={HomePageContainer} onEnter={loadCategorizedProducts} />
         <Route path="/cart" component={CartContainer} onEnter={loadProductsAndCartItems} />
         <Route path="/order" component={OrderContainer} onEnter={loadProductsAndCartItems}/>
+        <Route path="/admin" component={AdminContainer} />
 
       </Route>
     </Router>
