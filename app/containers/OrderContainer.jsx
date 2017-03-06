54b74f17e9130dae551d8ed6bc1808fa41cd2988
Aria Moraine <ariamoraine@gmail.com>
2017-03-06 14:00:53 -0500

    storing and updating local state of name and address

--- app/containers/OrderContainer.jsx
+++ app/containers/OrderContainer.jsx
@@ -1,5 +1,131 @@
 import { connect } from 'react-redux'
 import Order from '../components/Order'
+import React, { Component } from 'react'
+import { Grid, Row, Col, Button, FormControl, Form, FormGroup, ControlLabel } from 'react-bootstrap'
+import { Link } from 'react-router'
+//import OrderContainer from '../containers/OrderContainer'
+import { LinkContainer } from 'react-router-bootstrap'
+import ShippingAddressContainer from './ShippingAddressContainer'
+import ShippingAddress from "../components/ShippingAddress"
+
+
+
+class OrderContainer extends Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: "",
+      address: "",
+      enteredShipping: false,
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSave = this.handleSave.bind(this)
+  }
+
+  handleChange(input, event) {
+    const change = {};
+    const value = event.target.value
+    change[input] = value
+
+    this.setState(change)
+    console.log(this.state);
+  }
+
+  handleSave (event) {
+    event.preventDefault();
+    const temp = !this.state.enteredShipping
+    console.log(temp)
+    this.setState({enteredShipping: temp})
+    console.log("Saved???", this.state)
+  }
+
+  render() {
+    console.log(this.props)
+    let total = 0;
+    let lineItems = this.props.lineItems
+
+  let rows = lineItems && lineItems.map(item => {
+    let price = (item.product.price * item.quantity).toFixed(2);
+    total += +price;
+
+    return (
+      <div key={item.id} >
+        <Row className="show-grid">
+          <Col sm={2} md={2} >
+            <Link to={`products/${item.product.id}`}>
+              <img className="image-responsive" src={item.product.photo} />
+            </Link>
+          </Col>
+
+          <Col sm={5} md={5} >
+            <h3>{item.product.name}</h3>
+            <br />
+            <Form inline>
+              <FormGroup controlId="formInlineName">
+                <ControlLabel><h4>Quantity: {item.quantity}</h4></ControlLabel>
+                </FormGroup>
+            </Form>
+          </Col>
+
+          <Col sm={2} md={2}>
+            <h4>${price}</h4>
+          </Col>
+          <br />
+
+        </Row>
+        <hr />
+      </div>
+    )
+  })
+
+
+    return (
+      <Grid className="order">
+        <h1>Enter Shipping Address:</h1>
+             <ShippingAddress
+             handleSave={this.handleSave}
+             handleChange={this.handleChange}
+             state={this.state}
+              />
+
+        <br />
+
+        <h1>Review Items: </h1>
+          <Row className="show-grid">
+            <Col sm={2} md={2} />
+            <Col sm={2} md={5} />
+            <Col sm={2} md={2} />
+              <LinkContainer to="/cart" >
+                <Button>Edit Order</Button>
+              </LinkContainer>
+          </Row>
+
+         <Row className="show-grid">
+           { rows }
+         </Row>
+         <Row className="show-grid">
+           <Col sm={2} md={2} />
+           <Col sm={5} md={5} >
+             <h4>Total Price:</h4>
+           </Col>
+           <Col sm={2} md={2}>
+             <h4>{total}</h4>
+            </Col>
+            <LinkContainer to="/SOMETHINGHERE" >
+              <Button bsStyle='warning'>
+                Place Order
+              </Button>
+            </LinkContainer>
+
+         </Row>
+      </Grid>
+    )
+  }
+}
+
+
 
 export default connect(
   (state) => {
@@ -7,4 +133,4 @@ export default connect(
       lineItems: state.cart.lineItems,
     }
   }
-)(Order)
+)(OrderContainer)
